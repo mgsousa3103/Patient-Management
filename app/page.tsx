39167e9b0211e023bc8521372b75fea75a83a507
mgsousa3103 <matheusguedes.ufvjm.si@gmail.com>
2024-08-09 12:06:01 -0300

    Admin passcode verification

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 import PatientForm from "@/components/forms/PatientForm";
 import Link from "next/link";
+import PasskeyModal from "@/components/PasskeyModal";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+    const isAdmin = searchParams.admin === 'true'
     return (
         <div className="flex h-screen max-h-screen">
-            {/* TODO: OTP Verification | PasskeyModal */}
+            {isAdmin && <PasskeyModal />}
 
             <section className="remove-scrollbar container my-auto">
                 <div className="sub-container max-w-[496px]">
